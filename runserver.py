f55f71a167ca7c3669f956e4150590bef1a65328
Luis <luis90fuentes@gmail.com>
2018-12-13 23:01:32 -0800

    Site is live on heroku app!

--- runserver.py
+++ runserver.py
@@ -1,5 +1,7 @@
 from app import application
 
 if __name__ == '__main__':
+    # Added guard block to prevent multiple
+    # instances of flask from running
     #app.run(host='0.0.0.0')
     application.run()
